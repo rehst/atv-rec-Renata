fc75cf3367b7c8ec6111212becd305fd9e7272fa
rehst <107552363+rehst@users.noreply.github.com>
2024-11-29 09:54:54 -0300

    Update RedesSociaisMinhaEscola.js

--- graficos/RedesSociaisMinhaEscola.js
+++ graficos/RedesSociaisMinhaEscola.js
@@ -37,7 +37,7 @@ function processarDados(dados) {
         paper_bgcolor: getCSS('--bg-color'),
         height: 700,
         title: {
-            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            text: 'Redes sociais que as pessoas da minha escola mais utilizam',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
